dc55fa59d274cb614228c34589064578dbce6cde
Liliana Ortiz <marthaliliana33@gmail.com>
2018-10-24 11:24:23 +0200

    Class Init connect to Marklogic playground DB

---
+++ marklogicpoc/src/main/java/com/curuba/poc/marklogic/Init.java
@@ -0,0 +1,23 @@
+package com.curuba.poc.marklogic;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.document.JSONDocumentManager;
+import com.marklogic.client.io.JacksonHandle;
+import com.marklogic.client.DatabaseClientFactory.Authentication;
+
+/**POC to connect java with marklogic database*/
+public class Init {
+
+    public static void main(String... args) {
+        DatabaseClient client = DatabaseClientFactory.newClient("localhost", 8051, "admin", "admin",
+                Authentication.DIGEST);
+        JSONDocumentManager docMgr = client.newJSONDocumentManager();
+        JacksonHandle handle = new JacksonHandle();
+        docMgr.read("/fun-test/test/hello3.json", handle);
+        JsonNode doc = handle.get();
+        System.out.println(doc);
+        client.release();
+    }
+}
